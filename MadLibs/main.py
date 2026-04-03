ee915e2f1930ee7eda7b7498afc58d828345a520
Justin <jc.justinchau@gmail.com>
2026-04-02 22:18:51 -0400

    Create main.py

---
+++ MadLibs/main.py
@@ -0,0 +1,12 @@
+noun1 = input("Input a noun: ")
+noun2 = input("Input a noun: ")
+verb1 = input("Input a verb: ")
+verb2 = input("Input a verb: ")
+verb3 = input("Input a verb: ")
+
+print("Obi-wan: It's over Anakin, I have the " + noun1 + ".")
+print("Anakin Skywalker: You underestimate my " + noun2 + "!")
+print("Obi-wan: Don't " + verb1 + " it.")
+print("Obi-wan: You were the chosen one! It was said that you would \
+" + verb2 + " the Sith, not join them! Bring balance to the Force, \
+not " + verb3 + " it in darkness!")
